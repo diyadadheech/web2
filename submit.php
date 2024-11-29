d395a927069d99330fbc4e7756f184073688fdc4
diyadadheech <dida22cse@cmrit.ac.in>
2024-11-29 23:32:35 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,23 +1,13 @@
-$(document).ready(function() {
-    $('#submitBtn').click(function() {
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var phone = $('#phone').val();
-        var address = $('#address').val();
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $address = htmlspecialchars($_POST['address']);
 
-        if (name && email && phone && address) {
-            $('#resultName').text('Name: ' + name);
-            $('#resultEmail').text('Email: ' + email);
-            $('#resultPhone').text('Phone: ' + phone);
-            $('#resultAddress').text('Address: ' + address);
-            $('#result').show();
-        } else {
-            alert('Please fill all fields.');
-        }
-    });
-
-    $('#clearBtn').click(function() {
-        $('#registrationForm')[0].reset();
-        $('#result').hide();
-    });
-});
+    echo "Name: $name<br>";
+    echo "Email: $email<br>";
+    echo "Phone: $phone<br>";
+    echo "Address: $address<br>";
+}
+?>
