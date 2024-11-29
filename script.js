67e2f401e3b65d246515a02ec6c47e443debef11
diyadadheech <dida22cse@cmrit.ac.in>
2024-11-29 22:47:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#submitBtn').click(function() {
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+        var address = $('#address').val();
+
+        if(name && email && phone && address) {
+            $('#resultName').text('Name: ' + name);
+            $('#resultEmail').text('Email: ' + email);
+            $('#resultPhone').text('Phone: ' + phone);
+            $('#resultAddress').text('Address: ' + address);
+            $('#result').show();
+        } else {
+            alert('Please fill all fields.');
+        }
+    });
+});
